cfd8966240820856fe10da7469fda2699064bfca
Qayyax <qayyax@gmail.com>
2026-02-17 18:37:30 -0500

    created a commands folder to store slash commands

---
+++ commands/utility/ping.js
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "discord.js";
+
+const ping = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  async execute(interaction) {
+    await interaction.reply("Pong");
+  },
+};
+
+export default ping;
