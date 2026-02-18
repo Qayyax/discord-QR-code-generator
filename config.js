b9ee77ecb73b8fdeadd95485d01e183220cd9f11
Qayyax <qayyax@gmail.com>
2026-02-17 20:28:13 -0500

    added cliend and guild eid

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ import { configDotenv } from "dotenv";
 configDotenv({ quiet: true }); // This is to load .env variables using dotenv package
 const config = {
   discordToken: process.env.DISCORD_TOKEN,
+  clientID: process.env.CLIENT_ID,
+  guildID: process.env.GUILD_ID,
 };
 
 export default config;
