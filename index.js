cd84872829cd9b2e7f9f88e1ad8ff517280368b6
Qayyax <qayyax@gmail.com>
2026-02-17 20:03:26 -0500

    small chanes I don't know about

--- index.js
+++ index.js
@@ -1,10 +1,69 @@
+const fs = require("node:fs");
+const path = require("node:path");
 import config from "./config.js";
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  Collection,
+  CommandInteraction,
+  Events,
+  GatewayIntentBits,
+} from "discord.js";
 
 const { discordToken } = config;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in  as ${readyClient.user.id}`);
+});
+
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(folderPath);
+
+for (const folder of commandFolders) {
+  const commandPath = path.join(folderPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandPath)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandPath, file);
+    const command = require(filePath);
+
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing  required "data" or "execute" property.`,
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (!command) {
+    console.error(`No command matching  ${interaction.commandName} was found`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.log(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
 });
 
 client.login(discordToken);
